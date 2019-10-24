4183e959c4f696576e068063a86714c3e9e7682a
shun053013 <shun053013@gmail.com>
2019-10-24 15:18:23 +0900

    新登場

---
+++ New.java
@@ -0,0 +1,20 @@
+
+public class New {
+  private String cheracter;
+  private int level;
+  
+  
+public String getCheracter() {
+	return cheracter;
+}
+public void setCheracter(String cheracter) {
+	this.cheracter = cheracter;
+}
+public int getLevel() {
+	return level;
+}
+public void setLevel(int level) {
+	this.level = level;
+}
+  
+}
