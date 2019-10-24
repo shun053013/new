26d4b6324bdd2ee78b1504d8cb9205aa45b9ceb4
shun053013 <shun053013@gmail.com>
2019-10-24 16:35:02 +0900

    東俊輔

--- New.java
+++ New.java
@@ -3,6 +3,10 @@ public class New {
   private String cheracter;
   private int level;
   
+  public void name() {
+	  System.out.println("東俊輔");
+  }
+  
   
 public String getCheracter() {
 	return cheracter;
